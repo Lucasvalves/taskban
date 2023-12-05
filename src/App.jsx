85aeba4009c168da237c3078128ccfc6b1f6bfde
Lucas Veloso <lucasvelosoalves@gmail.com>
2023-12-05 08:19:50 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { Nav } from './components/Nav';
+
+function App() {
+  return (
+    <>
+      <Nav></Nav>
+      <h1 className="text-3xl font-bold underline text-red-700">
+        Hello world!
+      </h1>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Nav/index.jsx
@@ -0,0 +1,5 @@
+export const Nav = () => {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-dark">Nav bar</nav>
+  );
+};
