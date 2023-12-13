93135779869780b16c33f3a08741763a4c3a78e0
Lucas Veloso <lucasvelosoalves@gmail.com>
2023-12-13 15:45:25 -0300

    feat: editing tasks

--- src/App.jsx
+++ src/App.jsx
@@ -12,16 +12,17 @@ function App() {
       <AppProvider>
         <div className="bg-violet-50 h-screen w-screem flex">
   
-          <div className="w-full flex flex-row h-screen bg-orange-100">
+          <div className="w-full flex flex-row h-screen">
             <SideBar />
             <NavBar /> 
-            <div className="flex flex-col w-full bg-green-400">
-              <TaskCard taskName="To do"/>
-              <TaskCard taskName="Doing"/> 
-              <TaskCard taskName="QA" /> 
+            <div className="flex items-center mx-12 gap-6
+            6 w-full h-screen">
+              <TaskCard taskName="To do" className="flex-auto w-20"/>
+              <TaskCard taskName="Doing" className="flex-auto w-32"/> 
+              <TaskCard taskName="QA" className="flex-auto w-32"/> 
+              <TaskCard taskName="Done" className="flex-auto w-32"/> 
                
             </div>
-
             <Modal />                
           </div>
         </div>
--- src/components/Task/index.jsx
+++ src/components/Task/index.jsx
@@ -11,35 +11,32 @@ import { MdOutlineWatchLater } from "react-icons/md";
 export const TaskCard = ({taskName}) =>{
 
 	const {openToDo} = useAppContext()
-
+  // flex flex-col items-center gap-5     flex-auto
 
   if (openToDo) { 
     return (
-      <div className="flex flex-row items-center gap-5 w-full left-0 top-0 h-screen">
-        <div className="bg-violet-100 rounded-md h-fit w-[300px] drop-shadow-md ">
+        <div className="bg-violet-100 rounded-md h-fit  drop-shadow-md">
           <h1 className="text-stone-500 font-semibold text-xs mb-3 ml-3 mt-2 ">{taskName} (0)</h1>       
           <div className= "h-5/6 flex my-5  w-full flex-col items-center ">
             <div className=" bg-white h-1/6  rounded-md drop-shadow-md w-6/6 mx-3">
               <h1 className="text-stone-500 font-semibold text-base mb-3 ml-4 mt-4 ">Testar Navegadores</h1>
-              <p className="text-stone-500 text-sm m-4">
+              <p className="text-stone-500 text-xs m-4 h-fit  text-justify ">
                 Verificar e garantir a compatibilidade da
                 aplicação em diferentes navegadores.
               </p>
-              <div className=" flex flex-row m-4">
-                <span className="w-2/4  flex items-center flex-row gap-1 text-stone-500 ">
-                  <MdOutlineWatchLater />
-                  <span className=" text-stone-500 text-xs font-semibold" >
+              <div className=" flex flex-row mx-4 mb-4">
+                <span className="w-2/4  flex items-center flex-row text-stone-500 ">
+                  <MdOutlineWatchLater className="h-fit w-6 "/>
+                  <span className=" text-stone-500 text-[0.70rem] ml-1 font-semibold whitespace-nowrap" >
                     25 /12/2023
                   </span>
                 </span>
-                <span className="w-2/4 h-fit  flex justify-end">
+                <span className="w-2/4 h-6 flex justify-end ">
                   <span className="border border-green-400 text-green-400 bg-white
-                  text-xs pb-0.5 py-1 px-4 rounded-xl text-justify" >LOW</span>
+                    py-1 px-2.5 rounded-xl text-justify text-[0.70rem]" >LOW</span>
                 </span>
               </div>
             </div>
-            
-          </div>
         </div>
       </div>
     )
