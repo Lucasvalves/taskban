95357c08d0e5b4ebc6c01e364a1fafe1d0f96c15
Lucas Veloso <lucasvelosoalves@gmail.com>
2023-12-27 17:46:40 -0300

    ✨feat:  inserting drag and drop

--- src/components/TaskCard/index.jsx
+++ src/components/TaskCard/index.jsx
@@ -1,6 +1,7 @@
 import { MdOutlineWatchLater } from 'react-icons/md';
 import { handlePriorityColor } from '../../utils/constants';
 import CheckCircleOutlineIcon from '@mui/icons-material/CheckCircleOutline';
+import { Draggable } from "react-beautiful-dnd";
 
 export const TaskCard = ({ task, index, taskEntries }) => {
   const priorityClassName = handlePriorityColor(task.priority);
@@ -9,61 +10,53 @@ export const TaskCard = ({ task, index, taskEntries }) => {
     new Date(task.date) <= new Date() ? 'text-red-400' : 'text-stone-500 ';
 
   return (
-    //<Draggable key={task.id} draggableId={task.id} index={index}>
+    <Draggable key={task.id} draggableId={task.id} index={index}>
+      {(provided) => (    
+      <div className=" h-1/6 bg-white  rounded-lg drop-shadow-md w-full"
+        ref={provided.innerRef}
+        {...provided.draggableProps}
+        {...provided.dragHandleProps}    
+      >
+        <h1 className="text-stone-500 font-semibold text-base mb-3 ml-4 mt-4  ">
+          {task.title}
+        </h1>
+        <p className="text-stone-500 text-xs m-4 h-fit text-justify font-medium">
+          {task.description}
+        </p>
 
-    <div className=" h-1/6 bg-white  rounded-lg drop-shadow-md w-full">
-      <h1 className="text-stone-500 font-semibold text-base mb-3 ml-4 mt-4  ">
-        {task.title}
-      </h1>
-      <p className="text-stone-500 text-xs m-4 h-fit text-justify font-medium">
-        {task.description}
-      </p>
+        {taskEntries === 'Done' ? (
+          <div className=" flex gap-2 mx-4 mb-4 justify-start items-center">
+            <CheckCircleOutlineIcon className="h-fit w-6 text-green-400" />
 
-      {taskEntries === 'Done' ? (
-        <div className=" flex gap-2 mx-4 mb-4 justify-start items-center">
-          <CheckCircleOutlineIcon className="h-fit w-6 text-green-400" />
-
-          <span className="text-green-400   text-sm ">
-            Finalizado
-          </span>
-        </div>
-      ) : (
-        <div className=" w mx-4 mb-4 flex flex-row">
-          <span className="w-2/4 text-stone-500  ">
-            <span
-              className={`${textColor} flex flex-row text-[0.70rem] ml-1 font-semibold whitespace-nowrap items-center gap-2 text-xs`}
-              
-            >
-            <MdOutlineWatchLater className="h-fit w-6 " />
-
-              {new Date(task.date).toLocaleDateString('pt-BR')}
+            <span className="text-green-400   text-sm ">
+              Finalizado
             </span>
-          </span>
-          <span className="w-2/4 h-6 flex justify-end items-center">
-            <span
-              className={`${priorityClassName}  uppercase px-3 rounded-xl text-sm  `}
-            >
-              {task.priority}
+          </div>
+        ) : (
+          <div className=" w mx-4 mb-4 flex flex-row">
+            <span className="w-2/4 text-stone-500  ">
+              <span
+                className={`${textColor} flex flex-row text-[0.70rem] ml-1 font-semibold whitespace-nowrap items-center gap-2 text-xs`}
+                
+              >
+              <MdOutlineWatchLater className="h-fit w-6 " />
+
+                {new Date(task.date).toLocaleDateString('pt-BR')}
+              </span>
             </span>
-          </span>
-        </div>
-      )}
-    </div>
+            <span className="w-2/4 h-6 flex justify-end items-center">
+              <span
+                className={`${priorityClassName}  uppercase px-3 rounded-xl text-sm  `}
+              >
+                {task.priority}
+              </span>
+            </span>
+          </div>
+        )}
+      </div>
+       )}
+      </Draggable>
   );
 };
 
-//  <span className="w-2/4  flex items-center flex-row text-stone-500  ">
-// <MdOutlineWatchLater className="h-fit w-6 " />
-// <span
-//   className={`${textColor} text-[0.70rem] ml-1 font-semibold whitespace-nowrap`}
-// >
-//   {new Date(task.date).toLocaleDateString('pt-BR')}
-// </span>
-// </span>
-// <span className="w-2/4 h-6 flex justify-end items-center">
-// <span
-//   className={`${priorityClassName}  uppercase px-3 rounded-xl text-sm  `}
-// >
-//   {task.priority}
-// </span>
-// </span>
+
--- src/components/TaskContainer/index.jsx
+++ src/components/TaskContainer/index.jsx
@@ -1,35 +1,62 @@
 import { TaskCard } from '../TaskCard';
 import { useContext } from 'react';
 import { TaskList } from '../TaskList';
-import useModalContext from '../../hook/useModalContext';
 import { TasksContext } from '../../context/TasksContext';
 import {cardsTitle} from '../../utils/constants'
+import { DragDropContext, Droppable } from "react-beautiful-dnd";
 
 export const TaskContainer = () => {
-  const { tasks } = useContext(TasksContext);
+  const { tasks, setTasks } = useContext(TasksContext);
   const taskEntries = Array.from(Object.entries(cardsTitle));
 
+  const onDragEnd = (re) => {
+    if (!re.destination) return;
+    let newBoardData = tasks;
+   console.table(newBoardData)
+    var dragItem =
+      newBoardData[parseInt(re.source.droppableId)].items[re.source.index];
+      newBoardData[parseInt(re.source.droppableId)].items.splice(re.source.index,1);
+      newBoardData[parseInt(re.destination.droppableId)].items.splice(re.destination.index,0,dragItem);
+      setTasks(newBoardData.todo);
+  };
+
   return (
-    <div
-      className="6  h-full flex w-full justify-center overflow-y-scroll "
-    >
-      <div className="flex  justify-center gap-">
-        {tasks &&
-          taskEntries.map(([key, value]) => (
-            <TaskList key={key} title={`${value} (${tasks[key].length})`}>
-              {
-                // tasks[key].length > 0 ? (
-                tasks[key].map((task, index) => (
-                  <TaskCard key={task.id} task={task} index={index} taskEntries={value} />
-                ))
-                // ) : (
-                //   <p>Sem tarefas</p>
-                // )
-              }
-              {/* {provided.placeholder} */}
-            </TaskList>
-          ))}
+
+      <div
+        className="6  h-full flex w-full justify-center overflow-y-scroll "
+      >
+        <DragDropContext onDragEnd={onDragEnd}>
+          <div className="flex  justify-center gap-">
+            {tasks &&
+              taskEntries.map(([key, value], index) => (
+                <Droppable key={key} droppableId={key}>
+                {(provided, snapshot) => (
+                  <div
+                    {...provided.droppableProps}
+                    ref={provided.innerRef}
+                  >
+                {/* {console.log("Key: ",key,"Value: " ,value, "Index: ", index)} */}
+                
+                    <TaskList key={key} title={`${value} (${tasks[key].length})`}>
+                      {
+                        // tasks[key].length > 0 ? (
+                        tasks[key].map((task, index) => (
+                          <TaskCard key={task.id} task={task} index={index} taskEntries={value} />
+                        ))
+                        // ) : (
+                        //   <p>Sem tarefas</p>
+                        // )
+                      }
+                    </TaskList>
+                    {provided.placeholder} 
+                  </div>
+                  
+                  )}
+                  </Droppable>                
+              ))}
+          </div>
+        </DragDropContext>
       </div>
-    </div>
   );
 };
+   
